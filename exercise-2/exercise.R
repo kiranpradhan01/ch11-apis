095fc7904a5a5b25c38158726d0c0e16bd549c19
Kiran Pradhan <kpradhan@uw.edu>
2018-04-23 16:45:54 -0700

    use new york times API to get movie reviews

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -6,10 +6,10 @@ library(jsonlite)
 
 # Use `source()` to load your API key variable from the `apikey.R` file you made.
 # Make sure you've set your working directory!
-
+source("apikey.R")
 
 # Create a variable `movie.name` that is the name of a movie of your choice.
-
+movie_name <- "Harry Potter"
 
 # Construct an HTTP request to search for reviews for the given movie.
 # The base URI is `https://api.nytimes.com/svc/movies/v2/`
@@ -19,26 +19,39 @@ library(jsonlite)
 #
 # You should use YOUR api key (as the `api-key` parameter)
 # and your `movie.name` variable as the search query!
+base_uri <- "https://api.nytimes.com/svc/movies/v2/"
+resource <- "reviews/search.json"
+query_params <- list("api-key" = nty_apikey, "query" = movie_name)
 
+response <- GET(paste0(base_uri, resource), query = query_params)
 
 # Send the HTTP Request to download the data
 # Extract the content and convert it from JSON
-
+body <- content(response, "text")
+results <- fromJSON(body)
 
 # What kind of data structure did this produce? A data frame? A list?
-
+# a list consisting of a data frame of reviews
+class(results)
+is.data.frame(results)
 
 # Manually inspect the returned data and identify the content of interest 
 # (which are the movie reviews).
 # Use functions such as `names()`, `str()`, etc.
-
+names(results)
+str(results)
+names(results$results)
 
 # Flatten the movie reviews content into a data structure called `reviews`
-
+reviews <- flatten(results$results)
 
 # From the most recent review, store the headline, short summary, and link to
 # the full article, each in their own variables
-
+colnames(reviews)
+hp_recent_review <- reviews[1,]
+hp_headline <- hp_recent_review$headline
+hp_summary <- hp_recent_review$summary_short
+hp_link <- hp_recent_review$link.url
 
 # Create a list of the three pieces of information from above. 
 # Print out the list.
